b812752a713c1de8095a556053ea46a1243226b9
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-08-18 09:06:56 -0700

    Style flags

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -31,19 +31,33 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  const STYLES = {
+
+
+  }
+
+
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {/* <BaseFlag>{variant}</BaseFlag> */}
+          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+          {variant === 'new-release' && <NewFlag>Just released!</NewFlag>}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price style={{
+            '--color': variant === 'on-sale' && COLORS.gray[700],
+            '--text-decoration': variant === 'on-sale' && 'line-through',
+
+          }}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {variant === 'on-sale' ? <SalePrice>{formatPrice(salePrice)}</SalePrice> : undefined}
         </Row>
       </Wrapper>
     </Link>
@@ -56,13 +70,16 @@ const Link = styled.a`
 `;
 
 const Wrapper = styled.article`
-  width: 340px;
+  width: 344px;
+  height: 370px;
   display: flex;
   flex-direction: column;
+  margin-bottom: 30px;
 `;
 
 const ImageWrapper = styled.div`
   position: relative;
+  min-width: 320px;
 
 `;
 
@@ -72,6 +89,30 @@ const Image = styled.img`
   border-radius: 16px 16px 4px 4px;
 
 `;
+
+
+const BaseFlag = styled.div`
+  position: absolute;
+  color: ${COLORS.white};
+  right: -4px;
+  top: 12px;
+  padding: 0 10px;
+  height: 32px;
+  line-height: 32px;
+  font-size: ${14 / 16}rem;
+  font-weight: ${WEIGHTS.bold};
+  border-radius: 2px;
+`;
+
+const SaleFlag = styled(BaseFlag)`
+  background-color: ${COLORS.primary};
+`;
+
+const NewFlag = styled(BaseFlag)`
+  background-color: ${COLORS.secondary};
+`;
+
+
 
 const Row = styled.div`
   font-size: 1rem;
@@ -84,7 +125,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: var(--color);
+  text-decoration: var(--text-decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
